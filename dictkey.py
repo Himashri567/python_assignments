95694a98984d866317c543d2ce765cf116ba1622
Himashri567 <79900978+Himashri567@users.noreply.github.com>
2021-07-12 12:51:21 +0530

    Changing value of key of dictionary.

---
+++ dictkey.py
@@ -0,0 +1,6 @@
+#Can we update the value of a key in the dictionary? If yes, how?
+#ans: yes
+mydict = {"a": 1, "b": 2, "c":4}
+print("Before updating the value of key 'b': ",mydict)
+mydict["b"] = 3
+print("After updating value of 'b': ",mydict)
